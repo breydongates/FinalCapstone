7022f8b5f6dc849115d13f8ab411f83aab8fa2d7
Justin <jpduff85@gmail.com>
2020-12-07 16:34:23 -0500

    Some changes to SQL script

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -17,49 +17,73 @@ GO
 --create tables
 CREATE TABLE users (
 	user_id int IDENTITY(1,1) NOT NULL,
-	username varchar(50) NOT NULL,
-	password_hash varchar(200) NOT NULL,
-	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	username nvarchar(50) NOT NULL,
+	password_hash nvarchar(200) NOT NULL,
+	salt nvarchar(200) NOT NULL,
+	user_role nvarchar(50) NOT NULL
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
+
+CREATE TABLE users_collection (
+	collection_id int IDENTITY(1,1) NOT NULL, 
+	user_id int NOT NULL
+	CONSTRAINT PK_users_collection PRIMARY KEY (collection_id)
+)
+
 CREATE TABLE collections(
-	collection_id int IDENTITY(1,1) NOT NULL,
+	collection_id int NOT NULL,
 	is_private bit NOT NULL DEFAULT 0,
 	user_id int NOT NULL,
 	comic_id int NOT NULL
-	CONSTRAINT PK_collections PRIMARY KEY (collection_id, user_id, comic_id)
+	CONSTRAINT PK_collections PRIMARY KEY (collection_id, user_id, comic_id),
+	CONSTRAINT FK_users_collections FOREIGN KEY (collection_id) REFERENCES users_collection (collection_id),
 )
+
 CREATE TABLE comics (
-comic_id int IDENTITY(1,1) NOT NULL,
-title varchar (200) NOT NULL,
-comic_desc varchar (500),
-publisher varchar (100) NOT NULL,
+	comic_id int IDENTITY(1,1) NOT NULL,
+	title nvarchar (200) NOT NULL,
+	comic_desc nvarchar (500),
+	publisher nvarchar (100) NOT NULL,
+	CONSTRAINT PK_comics PRIMARY KEY (comic_id)
 )
 
 CREATE TABLE comic_creator (
-comic_id int NOT NULL,
-creator_id int NOT NULL
+	comic_id int NOT NULL,
+	creator_id int NOT NULL
+	CONSTRAINT FK_comic_creator_comics FOREIGN KEY (comic_id) REFERENCES comics (comic_id),
 )
 
 CREATE TABLE creators (
-creator_id int IDENTITY (1,1) NOT NULL,
-creator_name varchar (200)
+	creator_id int IDENTITY (1,1) NOT NULL,
+	creator_name varchar (200)
+	CONSTRAINT PK_creators PRIMARY KEY (creator_id)
 )
 
 CREATE TABLE comic_character (
-comic_id int NOT NULL,
-character_id int NOT NULL
+	comic_id int NOT NULL,
+	character_id int NOT NULL
+	CONSTRAINT FK_comic_character_comics FOREIGN KEY (comic_id) REFERENCES comics (comic_id),
+	
 )
 
 CREATE TABLE characters (
-character_id int IDENTITY (1,1) NOT NULL, 
-character_name varchar (200) NOT NULL
+	character_id int IDENTITY (1,1) NOT NULL, 
+	character_name nvarchar (200) NOT NULL
+	CONSTRAINT PK_characters PRIMARY KEY (character_id)
 )
 
 --populate default data: 'password'
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+ALTER TABLE collections 
+ADD CONSTRAINT FK_collections_comics FOREIGN KEY (comic_id) REFERENCES comics (comic_id)
+
+ALTER TABLE comic_creator
+ADD CONSTRAINT FK_comic_creator_creators FOREIGN KEY (creator_id) REFERENCES creators (creator_id)
+
+ALTER TABLE comic_character 
+ADD CONSTRAINT FK_comic_character_characters FOREIGN KEY (character_id) REFERENCES characters (character_id)
 
 GO
 
