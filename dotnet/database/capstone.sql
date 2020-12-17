db1ea2f902b13df0cfd94bc4665ae40ba5cd490b
Leslie <leslies.email3@gmail.com>
2020-12-17 13:57:39 -0500

    sql updates

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -75,8 +75,79 @@ ADD CONSTRAINT FK_collections_comics FOREIGN KEY (comic_id) REFERENCES comics (c
 ALTER TABLE comic_character 
 ADD CONSTRAINT FK_comic_character_characters FOREIGN KEY (character_id) REFERENCES characters (character_id)
 
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('3', 'FIRST VOLUMES', '1');
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('3', 'MY COMICS', '0');
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('4', 'BATMAN', '0');
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('4', 'SPIDER-MAN', '0');
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('4', 'BLACK PANTHER', '0');
+INSERT INTO users_collection (user_id, collection_name, is_private) VALUES('4', 'WONDER WOMAN', '0');
+
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('WOLVERINE: BLACK, WHITE, AND BLOOD', 'RETURN TO THE WEAPON X PROGRAM...', 'MARVEL', 'WOLVERINE', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('WONDER WOMAN: VOLUME 1 THE LIES REBIRTH','HEROIC. ICONIC. UNSTOPPABLE.  ARMED WITH HER LASSO OF TRUTH...', 'DC', 'WONDER WOMAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('HARLEY QUINN: VOLUME 1: HOT IN THE CITY', 'HARLEY QUINN RETURNS TO HER FIRST SOLO SERIES...', 'DC', 'HARLEY QUINN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BLACK WIDDOW #1', 'NATASHA ROMANOFF HAS BEEN A SPY ALMOST AS LONG...', 'MARVEL', 'BLACK WIDOW', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BATMAN VOL 1: THE COURT OF OWLS', 'AFTER A SERIES OF BRUTAL MURDERS ROCKS GOTHAM CITY...', 'DC', 'BATMAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('SUPERMAN #1', 'THE DOOMED PLANET KRYPTON WAS ABOUT TO EXPLODE...', 'DC', 'SUPERMAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('INCREDIBLE HULK VOL 1-1', 'LANDING IN A GRAVEYARD EN ROUTE TO NEW YORK...', 'MARVEL', 'HULK', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('AMAZING SPIDER-MAN', 'THE SPIDER-MAN EVENT OF THE DECADE BEGINS!', 'MARVEL', 'SPIDER-MAN', '667');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('HEROES AT HOME', 'BEING STUCK INSIDE ISN''T EASY FOR ANYONE, EVEN SUPER HEROES', 'MARVEL', 'SPIDER-MAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BLACK PANTHER: POWER TP', 'WAKANDA HAS BEEN ATTACKED ON EVERY FRONT...', 'MARVEL', 'BLACK PANTHER', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('YOUNG HELLBOY: THE HIDDEN LAND', 'STRANDED ON A STRANGE ISLAND AFTER A MISHAP...', 'DARK HORSE', 'HELLBOY', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('ASSASSINS CREED VALHALLA: SONG OF GLORY', 'EIVOR, A VIKING WARRIOR, OBSERVES A VILLAGE RAIDED...', 'DARK HORSE', 'EIVOR', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BATMAN AND ROBIN ETERNAL #1', 'FIVE YEARS AGO, BATMAN AND ROBIN WORKED THE MOST DISTURBING CASE...', 'DC', 'BATMAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BATMAN AND ROBIN ETERNAL #13', 'RED ROBIN AND RED HOOD HAVE FOLLOWED A TRAIL OF TECHNOLOGY...', 'DC', 'BATMAN', '13');
+
+
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '1');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '2');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '3');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '4');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '5');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '6');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('1', '3', '7');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '8');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '9');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '10');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '11');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '12');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '13');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('2', '3', '14');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('3', '4', '5');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('3', '4', '13');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('3', '4', '14');
+
+
+INSERT INTO characters (character_name) VALUES ('WOLVERINE');
+INSERT INTO characters (character_name) VALUES ('WONDER WOMAN');
+INSERT INTO characters (character_name) VALUES ('HARLEY QUINN');
+INSERT INTO characters (character_name) VALUES ('BLACK WIDOW');
+INSERT INTO characters (character_name) VALUES ('BATMAN');
+INSERT INTO characters (character_name) VALUES ('SUPERMAN');
+INSERT INTO characters (character_name) VALUES ('HULK');
+INSERT INTO characters (character_name) VALUES ('SPIDER-MAN');
+INSERT INTO characters (character_name) VALUES ('BLACK PANTHER');
+INSERT INTO characters (character_name) VALUES ('HELLBOY');
+INSERT INTO characters (character_name) VALUES ('EIVOR');
+
+INSERT INTO comic_character (comic_id, character_id) VALUES ('1', '1');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('2', '2');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('3', '3');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('4', '4');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('5', '5');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('6', '6');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('7', '7');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('8', '8');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('9', '8');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('10', '9');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('11', '10');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('12', '11');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('13', '5');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('14', '5');
+
 
 GO
+
+
 
 
 
---
+++ dotnet/database/standarduserdatasql.sql
@@ -0,0 +1,34 @@
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('3', '4', '15');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('4', '4', '8');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('4', '4', '16');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('4', '4', '17');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('5', '4', '18');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('5', '4', '19');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('5', '4', '20');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('6', '4', '21');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('6', '4', '22');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('6', '4', '23');
+INSERT INTO collections (collection_id, user_id, comic_id) VALUES ('6', '4', '24');
+
+INSERT INTO comic_character (comic_id, character_id) VALUES ('15', '5');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('16', '8');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('17', '8');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('18', '9');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('19', '9');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('20', '9');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('21', '2');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('22', '2');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('23', '2');
+INSERT INTO comic_character (comic_id, character_id) VALUES ('24', '2');
+
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BATMAN & ROBIN THE HUNT FOR ROBIN HC', 'AFTER LOOSING HIS SON DAMIAN - AKA ROBIN - BATMAN HAS FINALLY...', 'DC', 'BATMAN', '6');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('SPIDER-MAN #1', 'SPIDER-MAN SAVES A WOMAN FROM BEING MUGGED...', 'MARVEL', 'SPIDER-MAN', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('THE AMAZING SPIDER-MAN: RENEW YOUR VOWS', 'THE GREEN GOBLIN RETURNS!  NORMIE OSBORN HAS BEEN PLOTTING BEHIND THE SCENES...', 'MARVEL', '11');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('THE PUNISHER #1 BLACK PANTHER VARIANT', 'A BY-THE-NUMBERS DRUG BUST IS ABOUT TO TAKE FRANK CASTLE BY SURPRISE...', 'MARVEL', 'BLACK PANTHER', '1');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BLACK PANTHER RETURN OF THE DRAGON', 'BLACK PANTHER HEADS BACK TO NEW YORK', 'MARVEL', 'BLACK PANTHER', '38');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BLACK PANTHER: BACK TO AFRICA', 'OUT OF THE FRYING PAN...', 'MARVEL', 'BLACK PANTHER', '38');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('WONDER WOMAN #38', 'WONDER WOMAN''S WORLD  IS TURNED UPSIDE DOWN AS SHE FINDS HERSELF...', 'DC', 'WONDER WOMAN', '38');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('WONDER WOMAN #41', 'WONDER WOMAN TRAVELS ACROSS MOUNT OLYMPUS, FIRST VISITING DONNA TROY...', 'DC', 'WONDER WOMAN', '41');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('BLACKEST NIGHT: WONDER WOMAN', 'BLACK LANTERN MAXWELL LORD HAS RISEN AND HE SEEKS...', 'DC', 'WONDER WOMAN', '23');
+INSERT INTO comics (title, comic_desc, publisher, maincharacter, edition) VALUES('WONDER WOMAN: SPIRIT OF TRUTH', 'THIS BEAUTIFULLY PAINTED BOOK FINDS THE AMAZON PRINCESS HAVING A CRISIS OF FAITH...', 'DC', 'WONDER WOMAN', '12');
+
