0e6c17fd53fd17ec08ea1d130f2c73de174f0eff
Breydon <breydonagates@gmail.com>
2020-12-17 10:55:45 -0500

    pushing

--- vue/src/views/CollectionStats.vue
+++ vue/src/views/CollectionStats.vue
@@ -2,7 +2,7 @@
 
   <div class="yourCollections" style="overflow:hidden"> 
     <h1> Your Comics </h1>
-  <ul style="overflow-y:scroll; margin:0; height:100%;">
+  <ul style="overflow-y:scroll; margin:0; height:100%; padding-bottom:300px;">
         <view-comic v-bind:collectionId="collectionId">
     </view-comic>
    </ul> 
@@ -33,7 +33,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+    text-overflow: ellipsis;
+}
 
 </style>
